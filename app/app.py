83517b6053a8ed567241960212d16d83bc99f45a
Bhavin010 <makbhavin2001@gmail.com>
2026-04-02 14:53:14 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home(): 
    return jsonify({ 
        "status": "success", 
-       "message": "Test GitHub Actions workflow, session", 
+       "message": "Test GitHub Actions workflow session", 
        "environment": os.getenv('APP_ENV', 'development') 
    }) 
  
